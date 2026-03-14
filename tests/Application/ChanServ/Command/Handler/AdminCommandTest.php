6e865581a27a578aa8820c49bf1928e499b2d987
David Abuín Fontán <davidlig.es@gmail.com>
2026-03-14 10:05:33 +0100

    feat: add tests to improve coverage (ChanServ, MemoServ, NickServ)
    - AccessCommandTest: add tests for cannot_manage_level, add existing entry
      update, max_entries, del cannot_manage_level, del nick not registered,
      getter methods
    - AdminCommandTest: add full handler coverage (not supported, invalid channel,
      syntax, not registered, not identified, nick not registered, user not on
      channel, success, getters)
    - MemoServServiceTest: add tests for MemoDisabledException and Throwable
      catch blocks
    - NickProtectionServiceTest: add onNickChanged when not identified enforces
      protection
    - Update testing-coverage-priorities.md (1253 tests, 3582 assertions)

--- tests/Application/ChanServ/Command/Handler/AdminCommandTest.php
+++ tests/Application/ChanServ/Command/Handler/AdminCommandTest.php
@@ -18,7 +18,6 @@ use App\Domain\ChanServ\Repository\ChannelLevelRepositoryInterface;
 use App\Domain\ChanServ\Repository\RegisteredChannelRepositoryInterface;
 use App\Domain\NickServ\Entity\RegisteredNick;
 use App\Domain\NickServ\Repository\RegisteredNickRepositoryInterface;
-use App\Infrastructure\IRC\Protocol\Unreal\UnrealIRCdChannelModeSupport;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
@@ -33,7 +32,11 @@ final class AdminCommandTest extends TestCase
         array $args,
         ChanServNotifierInterface $notifier,
         TranslatorInterface $translator,
+        bool $hasAdminMode = true,
     ): ChanServContext {
+        $modeSupport = $this->createStub(\App\Application\Port\ChannelModeSupportInterface::class);
+        $modeSupport->method('hasAdmin')->willReturn($hasAdminMode);
+
         return new ChanServContext(
             $sender,
             $senderAccount,
@@ -46,19 +49,42 @@ final class AdminCommandTest extends TestCase
             'NOTICE',
             new ChanServCommandRegistry([]),
             $this->createStub(ChannelLookupPort::class),
-            new UnrealIRCdChannelModeSupport(),
+            $modeSupport,
         );
+    }
+
+    #[Test]
+    public function replyNotSupportedWhenChannelModeSupportHasNoAdmin(): void
+    {
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
+        $messages = [];
+        $notifier = $this->createStub(ChanServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
+            $messages[] = $m;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+
+        $cmd = new AdminCommand(
+            $this->createStub(RegisteredChannelRepositoryInterface::class),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, $account, ['#test', 'SomeNick'], $notifier, $translator, false));
+
+        self::assertSame(['admin.not_supported'], $messages);
     }
 
     #[Test]
     public function replyInvalidChannelWhenFirstArgNotChannel(): void
     {
-        $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
-        $nickRepo = $this->createStub(RegisteredNickRepositoryInterface::class);
-        $userLookup = $this->createStub(NetworkUserLookupPort::class);
-        $accessRepo = $this->createStub(ChannelAccessRepositoryInterface::class);
-        $levelRepo = $this->createStub(ChannelLevelRepositoryInterface::class);
-        $accessHelper = new ChanServAccessHelper($accessRepo, $levelRepo);
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
         $messages = [];
         $notifier = $this->createStub(ChanServNotifierInterface::class);
         $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
@@ -67,51 +93,276 @@ final class AdminCommandTest extends TestCase
         $translator = $this->createStub(TranslatorInterface::class);
         $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
 
-        $cmd = new AdminCommand($channelRepo, $nickRepo, $userLookup, $accessHelper);
-        $cmd->execute($this->createContext(new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip'), $this->createStub(RegisteredNick::class), ['x', 'Nick'], $notifier, $translator));
+        $cmd = new AdminCommand(
+            $this->createStub(RegisteredChannelRepositoryInterface::class),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, $account, ['notachannel', 'Nick'], $notifier, $translator));
 
         self::assertSame(['error.invalid_channel'], $messages);
     }
 
     #[Test]
-    public function successSetsAdminModeAndReplies(): void
+    public function replySyntaxWhenTargetNickEmpty(): void
     {
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
         $channel = $this->createStub(RegisteredChannel::class);
         $channel->method('getId')->willReturn(1);
+        $channel->method('getFounderNickId')->willReturn(1);
+        $channel->method('isFounder')->willReturn(true);
+        $channel->method('getName')->willReturn('#test');
         $channel->method('isSecure')->willReturn(false);
+
         $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
         $channelRepo->method('findByChannelName')->willReturn($channel);
-        $accessRepo = $this->createStub(ChannelAccessRepositoryInterface::class);
-        $access = $this->createStub(\App\Domain\ChanServ\Entity\ChannelAccess::class);
-        $access->method('getLevel')->willReturn(400);
-        $accessRepo->method('findByChannelAndNick')->willReturn($access);
-        $levelRepo = $this->createStub(ChannelLevelRepositoryInterface::class);
-        $levelRepo->method('findByChannelAndKey')->willReturn(null);
-        $accessHelper = new ChanServAccessHelper($accessRepo, $levelRepo);
-        $targetAccount = $this->createStub(RegisteredNick::class);
-        $nickRepo = $this->createStub(RegisteredNickRepositoryInterface::class);
-        $nickRepo->method('findByNick')->willReturn($targetAccount);
-        $userLookup = $this->createStub(NetworkUserLookupPort::class);
-        $userLookup->method('findByNick')->willReturn(new SenderView('UID2', 'TargetNick', 'i', 'h', 'c', 'ip'));
-        $account = $this->createStub(RegisteredNick::class);
-        $account->method('getId')->willReturn(1);
+
         $messages = [];
-        $modeCalls = [];
         $notifier = $this->createStub(ChanServNotifierInterface::class);
         $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
             $messages[] = $m;
         });
-        $notifier->method('setChannelMemberMode')->willReturnCallback(static function (string $ch, string $uid, string $letter, bool $add) use (&$modeCalls): void {
-            $modeCalls[] = [$ch, $uid, $letter, $add];
-        });
-        $notifier->method('sendNoticeToChannel')->willReturnCallback(static function (): void {});
         $translator = $this->createStub(TranslatorInterface::class);
         $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
 
-        $cmd = new AdminCommand($channelRepo, $nickRepo, $userLookup, $accessHelper);
-        $cmd->execute($this->createContext(new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip'), $account, ['#test', 'TargetNick'], $notifier, $translator));
+        $cmd = new AdminCommand(
+            $channelRepo,
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, $account, ['#test', ''], $notifier, $translator));
+
+        self::assertCount(1, $messages);
+        self::assertStringContainsString('error.syntax', $messages[0]);
+    }
+
+    #[Test]
+    public function throwsWhenChannelNotRegistered(): void
+    {
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
+        $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
+        $channelRepo->method('findByChannelName')->willReturn(null);
+
+        $notifier = $this->createStub(ChanServNotifierInterface::class);
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+
+        $cmd = new AdminCommand(
+            $channelRepo,
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+
+        $this->expectException(\App\Domain\ChanServ\Exception\ChannelNotRegisteredException::class);
+
+        $cmd->execute($this->createContext($sender, $account, ['#test', 'Nick'], $notifier, $translator));
+    }
+
+    #[Test]
+    public function replyNotIdentifiedWhenSenderAccountNull(): void
+    {
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $channel = $this->createStub(RegisteredChannel::class);
+        $channel->method('getId')->willReturn(1);
+        $channel->method('getFounderNickId')->willReturn(1);
+        $channel->method('isFounder')->willReturn(false);
+        $channel->method('getName')->willReturn('#test');
+        $channel->method('isSecure')->willReturn(false);
+
+        $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
+        $channelRepo->method('findByChannelName')->willReturn($channel);
+
+        $messages = [];
+        $notifier = $this->createStub(ChanServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
+            $messages[] = $m;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+
+        $cmd = new AdminCommand(
+            $channelRepo,
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, null, ['#test', 'Nick'], $notifier, $translator));
+
+        self::assertSame(['error.not_identified'], $messages);
+    }
+
+    #[Test]
+    public function replyNickNotRegisteredWhenTargetNotInDb(): void
+    {
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
+        $account->method('getId')->willReturn(1);
+        $channel = $this->createStub(RegisteredChannel::class);
+        $channel->method('getId')->willReturn(1);
+        $channel->method('getFounderNickId')->willReturn(1);
+        $channel->method('isFounder')->willReturn(true);
+        $channel->method('getName')->willReturn('#test');
+        $channel->method('isSecure')->willReturn(false);
+
+        $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
+        $channelRepo->method('findByChannelName')->willReturn($channel);
+        $nickRepo = $this->createStub(RegisteredNickRepositoryInterface::class);
+        $nickRepo->method('findByNick')->willReturn(null);
+
+        $messages = [];
+        $notifier = $this->createStub(ChanServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
+            $messages[] = $m;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+
+        $cmd = new AdminCommand(
+            $channelRepo,
+            $nickRepo,
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, $account, ['#test', 'UnregNick'], $notifier, $translator));
+
+        self::assertSame(['error.nick_not_registered'], $messages);
+    }
+
+    #[Test]
+    public function replyUserNotOnChannelWhenTargetNotOnNetwork(): void
+    {
+        $sender = new SenderView('UID1', 'User', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
+        $account->method('getId')->willReturn(1);
+        $channel = $this->createStub(RegisteredChannel::class);
+        $channel->method('getId')->willReturn(1);
+        $channel->method('getFounderNickId')->willReturn(1);
+        $channel->method('isFounder')->willReturn(true);
+        $channel->method('getName')->willReturn('#test');
+        $channel->method('isSecure')->willReturn(false);
+
+        $targetAccount = $this->createStub(RegisteredNick::class);
+        $targetAccount->method('getId')->willReturn(2);
+
+        $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
+        $channelRepo->method('findByChannelName')->willReturn($channel);
+        $nickRepo = $this->createStub(RegisteredNickRepositoryInterface::class);
+        $nickRepo->method('findByNick')->willReturn($targetAccount);
+        $userLookup = $this->createStub(NetworkUserLookupPort::class);
+        $userLookup->method('findByNick')->willReturn(null);
+
+        $messages = [];
+        $notifier = $this->createStub(ChanServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
+            $messages[] = $m;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+
+        $cmd = new AdminCommand(
+            $channelRepo,
+            $nickRepo,
+            $userLookup,
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, $account, ['#test', 'OnNetworkNick'], $notifier, $translator));
+
+        self::assertSame(['admin.user_not_on_channel'], $messages);
+    }
+
+    #[Test]
+    public function successGrantsAdminAndReplies(): void
+    {
+        $sender = new SenderView('UID1', 'Founder', 'i', 'h', 'c', 'ip');
+        $account = $this->createStub(RegisteredNick::class);
+        $account->method('getId')->willReturn(1);
+        $channel = $this->createStub(RegisteredChannel::class);
+        $channel->method('getId')->willReturn(1);
+        $channel->method('getFounderNickId')->willReturn(1);
+        $channel->method('isFounder')->willReturn(true);
+        $channel->method('getName')->willReturn('#test');
+        $channel->method('isSecure')->willReturn(false);
+
+        $targetAccount = $this->createStub(RegisteredNick::class);
+        $targetAccount->method('getId')->willReturn(2);
+        $targetSender = new SenderView('UID2', 'TargetNick', 'i', 'h', 'c', 'ip');
+
+        $channelRepo = $this->createStub(RegisteredChannelRepositoryInterface::class);
+        $channelRepo->method('findByChannelName')->willReturn($channel);
+        $nickRepo = $this->createStub(RegisteredNickRepositoryInterface::class);
+        $nickRepo->method('findByNick')->willReturn($targetAccount);
+        $userLookup = $this->createStub(NetworkUserLookupPort::class);
+        $userLookup->method('findByNick')->willReturn($targetSender);
+
+        $messages = [];
+        $notifier = $this->createMock(ChanServNotifierInterface::class);
+        $notifier->expects(self::once())->method('setChannelMemberMode')->with('#test', 'UID2', 'a', true);
+        $notifier->expects(self::once())->method('sendNoticeToChannel')->with(self::anything(), self::anything());
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $t, string $m) use (&$messages): void {
+            $messages[] = $m;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+
+        $cmd = new AdminCommand(
+            $channelRepo,
+            $nickRepo,
+            $userLookup,
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+        $cmd->execute($this->createContext($sender, $account, ['#test', 'TargetNick'], $notifier, $translator));
 
         self::assertSame(['admin.done'], $messages);
-        self::assertSame(['#test', 'UID2', 'a', true], $modeCalls[0]);
+    }
+
+    #[Test]
+    public function getterMethodsReturnExpectedValues(): void
+    {
+        $cmd = new AdminCommand(
+            $this->createStub(RegisteredChannelRepositoryInterface::class),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $this->createStub(NetworkUserLookupPort::class),
+            new ChanServAccessHelper(
+                $this->createStub(ChannelAccessRepositoryInterface::class),
+                $this->createStub(ChannelLevelRepositoryInterface::class),
+            ),
+        );
+
+        self::assertSame('ADMIN', $cmd->getName());
+        self::assertSame([], $cmd->getAliases());
+        self::assertSame(2, $cmd->getMinArgs());
+        self::assertSame('admin.syntax', $cmd->getSyntaxKey());
+        self::assertSame('admin.help', $cmd->getHelpKey());
+        self::assertSame(18, $cmd->getOrder());
+        self::assertSame('admin.short', $cmd->getShortDescKey());
+        self::assertSame([], $cmd->getSubCommandHelp());
+        self::assertFalse($cmd->isOperOnly());
+        self::assertSame('IDENTIFIED', $cmd->getRequiredPermission());
     }
 }
