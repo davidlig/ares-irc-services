1b7308e73ee40a90148f233672cde32423f58562
David Abuín Fontán <davidlig.es@gmail.com>
2026-03-14 07:09:00 +0100

    test(MemoServ): add MemoServContext and HelpFormatterContextAdapter tests

---
+++ tests/Application/MemoServ/Command/HelpFormatterContextAdapterTest.php
@@ -0,0 +1,230 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\MemoServ\Command;
+
+use App\Application\MemoServ\Command\HelpFormatterContextAdapter;
+use App\Application\MemoServ\Command\MemoServCommandInterface;
+use App\Application\MemoServ\Command\MemoServCommandRegistry;
+use App\Application\MemoServ\Command\MemoServContext;
+use App\Application\MemoServ\Command\MemoServNotifierInterface;
+use App\Application\Port\SenderView;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[CoversClass(HelpFormatterContextAdapter::class)]
+final class HelpFormatterContextAdapterTest extends TestCase
+{
+    #[Test]
+    public function replyDelegatesToContext(): void
+    {
+        $sent = [];
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $uid, string $msg) use (&$sent): void {
+            $sent[] = ['uid' => $uid, 'msg' => $msg];
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturn('Translated text');
+        $registry = new MemoServCommandRegistry([]);
+        $context = new MemoServContext(
+            new SenderView('UID1', 'N', 'i', 'h', 'c', 'ip'),
+            null,
+            'HELP',
+            [],
+            $notifier,
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            $registry,
+        );
+        $adapter = new HelpFormatterContextAdapter($context);
+
+        $adapter->reply('help.header_title');
+
+        self::assertCount(1, $sent);
+        self::assertSame('Translated text', $sent[0]['msg']);
+    }
+
+    #[Test]
+    public function replyRawDelegatesToContext(): void
+    {
+        $sent = [];
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $uid, string $msg) use (&$sent): void {
+            $sent[] = $msg;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $registry = new MemoServCommandRegistry([]);
+        $context = new MemoServContext(
+            new SenderView('UID1', 'N', 'i', 'h', 'c', 'ip'),
+            null,
+            'HELP',
+            [],
+            $notifier,
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            $registry,
+        );
+        $adapter = new HelpFormatterContextAdapter($context);
+
+        $adapter->replyRaw('Raw line');
+
+        self::assertCount(1, $sent);
+        self::assertSame('Raw line', $sent[0]);
+    }
+
+    #[Test]
+    public function transDelegatesToContextAndReturnsString(): void
+    {
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id): string => $id);
+        $context = new MemoServContext(
+            null,
+            null,
+            'HELP',
+            [],
+            $this->createStub(MemoServNotifierInterface::class),
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            new MemoServCommandRegistry([]),
+        );
+        $adapter = new HelpFormatterContextAdapter($context);
+
+        $result = $adapter->trans('help.syntax');
+
+        self::assertSame('help.syntax', $result);
+    }
+
+    #[Test]
+    public function getCommandsForGeneralHelpReturnsRegistryAll(): void
+    {
+        $handler = $this->createCommandStub('SEND');
+        $registry = new MemoServCommandRegistry([$handler]);
+        $context = new MemoServContext(
+            null,
+            null,
+            'HELP',
+            [],
+            $this->createStub(MemoServNotifierInterface::class),
+            $this->createStub(TranslatorInterface::class),
+            'en',
+            'UTC',
+            'NOTICE',
+            $registry,
+        );
+        $adapter = new HelpFormatterContextAdapter($context);
+
+        $commands = $adapter->getCommandsForGeneralHelp();
+
+        self::assertCount(1, [...$commands]);
+        $first = [...$commands][0];
+        self::assertSame($handler, $first);
+    }
+
+    #[Test]
+    public function shouldShowCommandInGeneralHelpReturnsTrueWhenNotOperOnly(): void
+    {
+        $command = $this->createCommandStub('LIST', false);
+        $adapter = new HelpFormatterContextAdapter($this->createMinimalContext());
+
+        self::assertTrue($adapter->shouldShowCommandInGeneralHelp($command));
+    }
+
+    #[Test]
+    public function shouldShowCommandInGeneralHelpReturnsFalseWhenOperOnly(): void
+    {
+        $command = $this->createCommandStub('ADMIN', true);
+        $adapter = new HelpFormatterContextAdapter($this->createMinimalContext());
+
+        self::assertFalse($adapter->shouldShowCommandInGeneralHelp($command));
+    }
+
+    private function createMinimalContext(): MemoServContext
+    {
+        return new MemoServContext(
+            null,
+            null,
+            'HELP',
+            [],
+            $this->createStub(MemoServNotifierInterface::class),
+            $this->createStub(TranslatorInterface::class),
+            'en',
+            'UTC',
+            'NOTICE',
+            new MemoServCommandRegistry([]),
+        );
+    }
+
+    private function createCommandStub(string $name, bool $operOnly = false): MemoServCommandInterface
+    {
+        return new class($name, $operOnly) implements MemoServCommandInterface {
+            public function __construct(
+                private readonly string $name,
+                private readonly bool $operOnly,
+            ) {
+            }
+
+            public function getName(): string
+            {
+                return $this->name;
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 0;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return '';
+            }
+
+            public function getHelpKey(): string
+            {
+                return '';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return '';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return $this->operOnly;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return null;
+            }
+
+            public function execute(MemoServContext $context): void
+            {
+            }
+        };
+    }
+}
---
+++ tests/Application/MemoServ/Command/MemoServContextTest.php
@@ -0,0 +1,226 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\MemoServ\Command;
+
+use App\Application\MemoServ\Command\MemoServCommandRegistry;
+use App\Application\MemoServ\Command\MemoServContext;
+use App\Application\MemoServ\Command\MemoServNotifierInterface;
+use App\Application\Port\SenderView;
+use DateTimeImmutable;
+use DateTimeZone;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[CoversClass(MemoServContext::class)]
+final class MemoServContextTest extends TestCase
+{
+    #[Test]
+    public function replyTranslatesAndSendsWhenSenderIsSet(): void
+    {
+        $sent = [];
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $uid, string $msg, string $type) use (&$sent): void {
+            $sent[] = ['uid' => $uid, 'msg' => $msg, 'type' => $type];
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id, array $params, string $domain, ?string $locale): string => $id . '|' . ($params['%name%'] ?? ''));
+        $registry = new MemoServCommandRegistry([]);
+        $sender = new SenderView('UID1', 'Nick', 'i', 'h', 'c', 'ip');
+        $context = new MemoServContext(
+            $sender,
+            null,
+            'SEND',
+            [],
+            $notifier,
+            $translator,
+            'en',
+            'Europe/Madrid',
+            'NOTICE',
+            $registry,
+        );
+
+        $context->reply('memo.sent', ['name' => 'Bob']);
+
+        self::assertCount(1, $sent);
+        self::assertSame('UID1', $sent[0]['uid']);
+        self::assertSame('memo.sent|Bob', $sent[0]['msg']);
+        self::assertSame('NOTICE', $sent[0]['type']);
+    }
+
+    #[Test]
+    public function replyDoesNotSendWhenSenderIsNull(): void
+    {
+        $sent = [];
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function () use (&$sent): void {
+            $sent[] = true;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturn('translated');
+        $registry = new MemoServCommandRegistry([]);
+        $context = new MemoServContext(
+            null,
+            null,
+            'SEND',
+            [],
+            $notifier,
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            $registry,
+        );
+
+        $context->reply('some.key');
+
+        self::assertCount(0, $sent);
+    }
+
+    #[Test]
+    public function replyRawSendsMessageWhenSenderIsSet(): void
+    {
+        $sent = [];
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function (string $uid, string $msg) use (&$sent): void {
+            $sent[] = ['uid' => $uid, 'msg' => $msg];
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $registry = new MemoServCommandRegistry([]);
+        $sender = new SenderView('UID2', 'User', 'i', 'h', 'c', 'ip');
+        $context = new MemoServContext(
+            $sender,
+            null,
+            'READ',
+            [],
+            $notifier,
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            $registry,
+        );
+
+        $context->replyRaw('Raw line');
+
+        self::assertCount(1, $sent);
+        self::assertSame('UID2', $sent[0]['uid']);
+        self::assertSame('Raw line', $sent[0]['msg']);
+    }
+
+    #[Test]
+    public function replyRawDoesNotSendWhenSenderIsNull(): void
+    {
+        $sent = [];
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $notifier->method('sendMessage')->willReturnCallback(static function () use (&$sent): void {
+            $sent[] = true;
+        });
+        $translator = $this->createStub(TranslatorInterface::class);
+        $registry = new MemoServCommandRegistry([]);
+        $context = new MemoServContext(
+            null,
+            null,
+            'LIST',
+            [],
+            $notifier,
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            $registry,
+        );
+
+        $context->replyRaw('Anything');
+
+        self::assertCount(0, $sent);
+    }
+
+    #[Test]
+    public function gettersReturnInjectedValues(): void
+    {
+        $notifier = $this->createStub(MemoServNotifierInterface::class);
+        $translator = $this->createStub(TranslatorInterface::class);
+        $registry = new MemoServCommandRegistry([]);
+        $context = new MemoServContext(
+            null,
+            null,
+            'HELP',
+            ['arg1'],
+            $notifier,
+            $translator,
+            'es',
+            'Europe/Madrid',
+            'PRIVMSG',
+            $registry,
+        );
+
+        self::assertSame($notifier, $context->getNotifier());
+        self::assertSame('es', $context->getLanguage());
+        self::assertSame('Europe/Madrid', $context->getTimezone());
+        self::assertSame($registry, $context->getRegistry());
+        self::assertSame('HELP', $context->command);
+        self::assertSame(['arg1'], $context->args);
+    }
+
+    #[Test]
+    public function formatDateReturnsDashWhenDateIsNull(): void
+    {
+        $context = $this->createMinimalContext();
+
+        self::assertSame('—', $context->formatDate(null));
+    }
+
+    #[Test]
+    public function formatDateFormatsWithContextTimezone(): void
+    {
+        $context = $this->createMinimalContext('UTC');
+        $date = new DateTimeImmutable('2025-03-14 12:00:00', new DateTimeZone('UTC'));
+
+        $formatted = $context->formatDate($date);
+
+        self::assertSame('14/03/2025 12:00 UTC', $formatted);
+    }
+
+    #[Test]
+    public function transReturnsTranslationWithWrappedParams(): void
+    {
+        $translator = $this->createStub(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(static fn (string $id, array $params): string => $id . ':' . ($params['%nick%'] ?? ''));
+        $context = new MemoServContext(
+            null,
+            null,
+            'SEND',
+            [],
+            $this->createStub(MemoServNotifierInterface::class),
+            $translator,
+            'en',
+            'UTC',
+            'NOTICE',
+            new MemoServCommandRegistry([]),
+        );
+
+        $result = $context->trans('memo.from', ['nick' => 'Alice']);
+
+        self::assertSame('memo.from:Alice', $result);
+    }
+
+    private function createMinimalContext(string $timezone = 'UTC'): MemoServContext
+    {
+        return new MemoServContext(
+            null,
+            null,
+            'LIST',
+            [],
+            $this->createStub(MemoServNotifierInterface::class),
+            $this->createStub(TranslatorInterface::class),
+            'en',
+            $timezone,
+            'NOTICE',
+            new MemoServCommandRegistry([]),
+        );
+    }
+}
