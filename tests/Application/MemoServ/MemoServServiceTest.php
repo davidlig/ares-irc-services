5c1e68345f6e5219a295d89a5d596de711caa2e3
David Abuín Fontán <davidlig.es@gmail.com>
2026-03-14 05:55:56 +0100

    test: NickServService and MemoServService dispatch guards (empty, operOnly, notIdentified, syntax); docs: update test count

--- tests/Application/MemoServ/MemoServServiceTest.php
+++ tests/Application/MemoServ/MemoServServiceTest.php
@@ -159,4 +159,271 @@ final class MemoServServiceTest extends TestCase
         $service->dispatch('   ', $sender);
         $service->dispatch('', $sender);
     }
+
+    #[Test]
+    public function repliesOperOnlyWhenHandlerIsOperOnly(): void
+    {
+        $sender = new SenderView('UID1', 'Nick', 'ident', 'host', 'cloak', 'ip', true, false, '001', 'cloak');
+        $contextHolder = new stdClass();
+        $contextHolder->context = null;
+
+        $operOnlyHandler = new class($contextHolder) implements MemoServCommandInterface {
+            public function __construct(private readonly stdClass $holder)
+            {
+            }
+
+            public function getName(): string
+            {
+                return 'OPCMD';
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 0;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return 'syntax';
+            }
+
+            public function getHelpKey(): string
+            {
+                return 'help';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return 'short';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return true;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return null;
+            }
+
+            public function execute(MemoServContext $context): void
+            {
+                $this->holder->context = $context;
+            }
+        };
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(
+            static fn (string $id): string => 'error.oper_only' === $id ? 'Oper only' : $id
+        );
+        $notifier = $this->createMock(MemoServNotifierInterface::class);
+        $notifier->expects(self::once())->method('sendMessage')->with($sender->uid, 'Oper only', 'NOTICE');
+
+        $service = new MemoServService(
+            new MemoServCommandRegistry([$operOnlyHandler]),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $notifier,
+            new UserMessageTypeResolver($this->createStub(RegisteredNickRepositoryInterface::class)),
+            $translator,
+        );
+
+        $service->dispatch('OPCMD', $sender);
+
+        self::assertNull($contextHolder->context);
+    }
+
+    #[Test]
+    public function repliesNotIdentifiedWhenRequiredPermissionIdentifiedAndNoAccount(): void
+    {
+        $sender = new SenderView('UID1', 'Nick', 'ident', 'host', 'cloak', 'ip', true, false, '001', 'cloak');
+        $contextHolder = new stdClass();
+        $contextHolder->context = null;
+
+        $identifiedHandler = new class($contextHolder) implements MemoServCommandInterface {
+            public function __construct(private readonly stdClass $holder)
+            {
+            }
+
+            public function getName(): string
+            {
+                return 'NEEDID';
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 0;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return 'syntax';
+            }
+
+            public function getHelpKey(): string
+            {
+                return 'help';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return 'short';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return false;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return 'IDENTIFIED';
+            }
+
+            public function execute(MemoServContext $context): void
+            {
+                $this->holder->context = $context;
+            }
+        };
+
+        $nickRepository = $this->createStub(RegisteredNickRepositoryInterface::class);
+        $nickRepository->method('findByNick')->willReturn(null);
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(
+            static fn (string $id): string => 'error.not_identified' === $id ? 'Not identified' : $id
+        );
+        $notifier = $this->createMock(MemoServNotifierInterface::class);
+        $notifier->expects(self::once())->method('sendMessage')->with($sender->uid, 'Not identified', 'NOTICE');
+
+        $service = new MemoServService(
+            new MemoServCommandRegistry([$identifiedHandler]),
+            $nickRepository,
+            $notifier,
+            new UserMessageTypeResolver($nickRepository),
+            $translator,
+        );
+
+        $service->dispatch('NEEDID', $sender);
+
+        self::assertNull($contextHolder->context);
+    }
+
+    #[Test]
+    public function repliesSyntaxWhenArgsBelowMinArgs(): void
+    {
+        $sender = new SenderView('UID1', 'Nick', 'ident', 'host', 'cloak', 'ip', true, false, '001', 'cloak');
+        $contextHolder = new stdClass();
+        $contextHolder->context = null;
+
+        $minArgsHandler = new class($contextHolder) implements MemoServCommandInterface {
+            public function __construct(private readonly stdClass $holder)
+            {
+            }
+
+            public function getName(): string
+            {
+                return 'TWOARGS';
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 2;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return 'syntax.twoargs';
+            }
+
+            public function getHelpKey(): string
+            {
+                return 'help';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return 'short';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return false;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return null;
+            }
+
+            public function execute(MemoServContext $context): void
+            {
+                $this->holder->context = $context;
+            }
+        };
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(
+            static fn (string $id, array $params = []): string => 'error.syntax' === $id ? 'Syntax: ' . ($params['syntax'] ?? '') : $id
+        );
+        $notifier = $this->createMock(MemoServNotifierInterface::class);
+        $notifier->expects(self::once())->method('sendMessage')->with($sender->uid, self::stringContains('Syntax:'), 'NOTICE');
+
+        $service = new MemoServService(
+            new MemoServCommandRegistry([$minArgsHandler]),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $notifier,
+            new UserMessageTypeResolver($this->createStub(RegisteredNickRepositoryInterface::class)),
+            $translator,
+        );
+
+        $service->dispatch('TWOARGS onlyone', $sender);
+
+        self::assertNull($contextHolder->context);
+    }
 }
