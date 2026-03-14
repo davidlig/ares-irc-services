5bf4f90b4924c9a471732b52b0b6319be164ca23
David Abuín Fontán <davidlig.es@gmail.com>
2026-03-14 05:55:56 +0100

    test: NickServService and MemoServService dispatch guards (empty, operOnly, notIdentified, syntax); docs: update test count

--- tests/Application/NickServ/NickServServiceTest.php
+++ tests/Application/NickServ/NickServServiceTest.php
@@ -11,6 +11,7 @@ use App\Application\NickServ\Command\NickServNotifierInterface;
 use App\Application\NickServ\NickServService;
 use App\Application\NickServ\Security\AuthorizationCheckerInterface;
 use App\Application\NickServ\Security\AuthorizationContextInterface;
+use App\Application\NickServ\Security\NickServPermission;
 use App\Application\Port\SenderView;
 use App\Domain\NickServ\Entity\RegisteredNick;
 use App\Domain\NickServ\Repository\RegisteredNickRepositoryInterface;
@@ -181,5 +182,316 @@ final class NickServServiceTest extends TestCase
         );
 
         $service->dispatch('UNKNOWN arg', $sender);
+    }
+
+    #[Test]
+    public function emptyCommandDoesNothing(): void
+    {
+        $sender = new SenderView('UID1', 'N', 'i', 'h', 'c', '127.0.0.1');
+        $notifier = $this->createMock(NickServNotifierInterface::class);
+        $notifier->expects(self::never())->method('sendMessage');
+
+        $service = new NickServService(
+            $this->createStub(AuthorizationContextInterface::class),
+            $this->createStub(AuthorizationCheckerInterface::class),
+            new NickServCommandRegistry([]),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $notifier,
+            new UserMessageTypeResolver($this->createStub(RegisteredNickRepositoryInterface::class)),
+            $this->createStub(TranslatorInterface::class),
+            new \App\Application\NickServ\PendingVerificationRegistry(),
+            new \App\Application\NickServ\RecoveryTokenRegistry(),
+        );
+
+        $service->dispatch('   ', $sender);
+        $service->dispatch('', $sender);
+    }
+
+    #[Test]
+    public function repliesOperOnlyWhenHandlerIsOperOnlyAndNotGranted(): void
+    {
+        $sender = new SenderView('UID1', 'Nick', 'ident', 'host', 'cloak', '127.0.0.1', true, false, '001', 'cloak');
+        $contextHolder = new stdClass();
+        $contextHolder->context = null;
+
+        $operOnlyHandler = new class($contextHolder) implements NickServCommandInterface {
+            public function __construct(private readonly stdClass $holder)
+            {
+            }
+
+            public function getName(): string
+            {
+                return 'OPCMD';
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 0;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return 'syntax';
+            }
+
+            public function getHelpKey(): string
+            {
+                return 'help';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return 'short';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return true;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return null;
+            }
+
+            public function execute(NickServContext $context): void
+            {
+                $this->holder->context = $context;
+            }
+        };
+
+        $authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
+        $authorizationChecker->expects(self::once())
+            ->method('isGranted')
+            ->with(NickServPermission::NETWORK_OPER, self::anything())
+            ->willReturn(false);
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(
+            static fn (string $id): string => 'error.oper_only' === $id ? 'Oper only' : $id
+        );
+        $notifier = $this->createMock(NickServNotifierInterface::class);
+        $notifier->expects(self::once())->method('sendMessage')->with($sender->uid, 'Oper only', 'NOTICE');
+
+        $service = new NickServService(
+            $this->createStub(AuthorizationContextInterface::class),
+            $authorizationChecker,
+            new NickServCommandRegistry([$operOnlyHandler]),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $notifier,
+            new UserMessageTypeResolver($this->createStub(RegisteredNickRepositoryInterface::class)),
+            $translator,
+            new \App\Application\NickServ\PendingVerificationRegistry(),
+            new \App\Application\NickServ\RecoveryTokenRegistry(),
+        );
+
+        $service->dispatch('OPCMD', $sender);
+
+        self::assertNull($contextHolder->context);
+    }
+
+    #[Test]
+    public function repliesNotIdentifiedWhenRequiredPermissionNotGranted(): void
+    {
+        $sender = new SenderView('UID1', 'Nick', 'ident', 'host', 'cloak', '127.0.0.1', true, false, '001', 'cloak');
+        $contextHolder = new stdClass();
+        $contextHolder->context = null;
+
+        $identifiedHandler = new class($contextHolder) implements NickServCommandInterface {
+            public function __construct(private readonly stdClass $holder)
+            {
+            }
+
+            public function getName(): string
+            {
+                return 'NEEDID';
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 0;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return 'syntax';
+            }
+
+            public function getHelpKey(): string
+            {
+                return 'help';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return 'short';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return false;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return NickServPermission::IDENTIFIED_OWNER;
+            }
+
+            public function execute(NickServContext $context): void
+            {
+                $this->holder->context = $context;
+            }
+        };
+
+        $authorizationChecker = $this->createMock(AuthorizationCheckerInterface::class);
+        $authorizationChecker->expects(self::once())
+            ->method('isGranted')
+            ->with(NickServPermission::IDENTIFIED_OWNER, self::anything())
+            ->willReturn(false);
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(
+            static fn (string $id): string => 'error.not_identified' === $id ? 'Not identified' : $id
+        );
+        $notifier = $this->createMock(NickServNotifierInterface::class);
+        $notifier->expects(self::once())->method('sendMessage')->with($sender->uid, 'Not identified', 'NOTICE');
+
+        $service = new NickServService(
+            $this->createStub(AuthorizationContextInterface::class),
+            $authorizationChecker,
+            new NickServCommandRegistry([$identifiedHandler]),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $notifier,
+            new UserMessageTypeResolver($this->createStub(RegisteredNickRepositoryInterface::class)),
+            $translator,
+            new \App\Application\NickServ\PendingVerificationRegistry(),
+            new \App\Application\NickServ\RecoveryTokenRegistry(),
+        );
+
+        $service->dispatch('NEEDID', $sender);
+
+        self::assertNull($contextHolder->context);
+    }
+
+    #[Test]
+    public function repliesSyntaxWhenArgsBelowMinArgs(): void
+    {
+        $sender = new SenderView('UID1', 'Nick', 'ident', 'host', 'cloak', '127.0.0.1', true, false, '001', 'cloak');
+        $contextHolder = new stdClass();
+        $contextHolder->context = null;
+
+        $minArgsHandler = new class($contextHolder) implements NickServCommandInterface {
+            public function __construct(private readonly stdClass $holder)
+            {
+            }
+
+            public function getName(): string
+            {
+                return 'TWOARGS';
+            }
+
+            public function getAliases(): array
+            {
+                return [];
+            }
+
+            public function getMinArgs(): int
+            {
+                return 2;
+            }
+
+            public function getSyntaxKey(): string
+            {
+                return 'syntax.twoargs';
+            }
+
+            public function getHelpKey(): string
+            {
+                return 'help';
+            }
+
+            public function getOrder(): int
+            {
+                return 0;
+            }
+
+            public function getShortDescKey(): string
+            {
+                return 'short';
+            }
+
+            public function getSubCommandHelp(): array
+            {
+                return [];
+            }
+
+            public function isOperOnly(): bool
+            {
+                return false;
+            }
+
+            public function getRequiredPermission(): ?string
+            {
+                return null;
+            }
+
+            public function execute(NickServContext $context): void
+            {
+                $this->holder->context = $context;
+            }
+        };
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator->method('trans')->willReturnCallback(
+            static fn (string $id, array $params = []): string => 'error.syntax' === $id ? 'Syntax: ' . ($params['syntax'] ?? '') : $id
+        );
+        $notifier = $this->createMock(NickServNotifierInterface::class);
+        $notifier->expects(self::once())->method('sendMessage')->with($sender->uid, self::stringContains('Syntax:'), 'NOTICE');
+
+        $service = new NickServService(
+            $this->createStub(AuthorizationContextInterface::class),
+            $this->createStub(AuthorizationCheckerInterface::class),
+            new NickServCommandRegistry([$minArgsHandler]),
+            $this->createStub(RegisteredNickRepositoryInterface::class),
+            $notifier,
+            new UserMessageTypeResolver($this->createStub(RegisteredNickRepositoryInterface::class)),
+            $translator,
+            new \App\Application\NickServ\PendingVerificationRegistry(),
+            new \App\Application\NickServ\RecoveryTokenRegistry(),
+        );
+
+        $service->dispatch('TWOARGS onlyone', $sender);
+
+        self::assertNull($contextHolder->context);
     }
 }
