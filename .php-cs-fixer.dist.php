99b6822d49326a784c764fc173e4b3559166a38f
David Abuín Fontán <davidlig.es@gmail.com>
2026-08-19 00:08:16 +0200

    chore(config): update dependencies and project configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,12 +15,9 @@ return (new PhpCsFixer\Config())
         '@Symfony' => true,
         '@Symfony:risky' => true,
 
-        // Specific rules for modern PHP (8.4+)
-        '@PHP80Migration:risky' => true,
-        '@PHP81Migration' => true,
-        '@PHP82Migration' => true,
-        '@PHP83Migration' => true,
-        '@PHP84Migration' => true,
+        // Specific rules for modern PHP (8.5+)
+        '@PHP8x5Migration' => true,
+        '@PHP8x5Migration:risky' => true,
 
         // Mandatory strict typing
         'declare_strict_types' => true,
@@ -51,6 +48,9 @@ return (new PhpCsFixer\Config())
             'import_classes' => true,
             'import_constants' => true,
             'import_functions' => true,
+        ],
+        'fully_qualified_strict_types' => [
+            'import_symbols' => true,
         ],
         'ordered_imports' => [
             'sort_algorithm' => 'alpha',
